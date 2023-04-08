5243d53737047dae025de4b86315d41afac87a07
Danny Nguyen <dannynvn@gmail.com>
2023-04-08 15:38:50 -0700

    built out modal user input fields and connected to createProject.js

--- public/js/createProject.js
+++ public/js/createProject.js
@@ -2,12 +2,16 @@ const newFormHandler = async (event) => {
     event.preventDefault();
   
     const name = document.querySelector('#projectName').value.trim();
+    const clientName = document.querySelector('#clientName').value.trim();
     const description = document.querySelector('#projectDescription').value.trim();
+    const dueDate = document.querySelector('#projectDue').value.trim();
+    const hours = document.querySelector('#projectHours').value.trim();
+    const rate = document.querySelector('#projectRate').value.trim();
   
     if (name && description) {
       const response = await fetch(`/api/projects`, {
         method: 'POST',
-        body: JSON.stringify({ name, description }),
+        body: JSON.stringify({ name, clientName, description, dueDate, hours, rate }),
         headers: {
           'Content-Type': 'application/json',
         },
