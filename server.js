490fc2982e200a5f6915b75b1d9ef743b0e99e59
Aubrey Johnston <aubreyjohnston@Aubreys-MacBook-Air.local>
2023-04-10 18:51:28 -0700

    sequelize

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const routes = require('./controllers');
 const helpers = require('./utils/helper');
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
