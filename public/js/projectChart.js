bf65607c1d20cfe5c1c116c8222642f88ac69ca3
Travis <traviscmiller01@gmail.com>
2023-04-11 15:11:19 -0700

    created hours pie chart and earnings bar chart

--- public/js/projectChart.js
+++ public/js/projectChart.js
@@ -1,42 +1,48 @@
-const projectChart = document.getElementById('projectChart');
+const hoursChart = document.getElementById("projectChart");
 
 const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
+  { name: "Project 1", hours: 14 },
+  { name: "Project 2", hours: 23 },
+  { name: "Project 3", hours: 30 },
+  { name: "Project 4", hours: 43 },
+  { name: "Project 5", hours: 75 },
 ];
-        
-new Chart(projectChart, {
-    type: 'bar',
-    options: {
-        animation: false,
-        plugins: {
-            legend: {
-                display: false
-            },
-            tooltip: {
-                enabled: false
-            }
-        }
-    },
-    data: {
-        labels: data.map(row => row.year),
-        datasets: [{
-            label: 'Acquisitions by Year',
-            data: data.map(row => row.count),
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
-    }
-});
 
+new Chart(hoursChart, {
+  type: "pie",
+  options: {
+    responsive: true,
+    animation: false,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      scales: {
+        y: {
+          beginAtZero: true,
+          ticks: {
+            display: false,
+          },
+        },
+      },
+    },
+  },
+  data: {
+    labels: data.map((row) => row.name),
+    datasets: [
+      {
+        label: "Hours Worked",
+        data: data.map((row) => row.hours),
+        borderWidth: 1,
+        backgroundColor: [
+          "#c6beb5",
+          "#6aa2b8",
+          "#555759",
+          " #f78d2d",
+          "#1b3d6d",
+          "#7ab800",
+        ],
+      },
+    ],
+  },
+});
