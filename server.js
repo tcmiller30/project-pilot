c6f96579fc9070f29e12c8e3a58171f8906c6715
Aubrey Johnston <aubreyjohnston@Aubreys-MacBook-Air.local>
2023-04-10 19:36:22 -0700

    fixed hbs bug

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars')
 
