9d47f6fa46743281f5b6d220275627c542316938
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-04-11 17:47:57 -0600

    minor changes to create consistency

--- controllers/dashboardRoute.js
+++ controllers/dashboardRoute.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Project } = require('../models');
 const withAuth = require('../utils/auth');
 
-//Login Page that will contain the sign in/up form
+//Dashboard displays all the projects
 router.get('/', withAuth, async (req, res) => {
 try {
     // Getting the card that shows the project and grabs users name and due date.
@@ -12,10 +12,13 @@ try {
     });
     // Create a new array for all the projects need to add
     const allProjects = projectCards.map((project) => project.get({ plain: true }));
+    console.log(allProjects);
+
     res.render('dashboard', {
         allProjects,
-    // Need authrotized js to know if theyre logged in
-      user_id: req.session.user_id
+    // Need authorized js to know if theyre logged in
+    //   user_id: req.session.user_id
+    logged_in: req.session.logged_in 
     })
     // res.status(200).json(projectCards)
 } catch (error) {
@@ -35,7 +38,7 @@ router.get('/project/:id', withAuth, async (req, res) => {
 
     res.render('project', {
         ...project,
-       // Need authrotized js to know if theyre logged in
+       // Need authorized js to know if theyre logged in
       user_id: req.session.user_id
     });
  } catch (error) {
@@ -46,7 +49,7 @@ router.get('/project/:id', withAuth, async (req, res) => {
 router.get('/login', async (req, res) => {
     try {
         if(req.session.loggedIn) {
-            res.redirect('/dashboard');
+            res.redirect('/');
             return
         } else {
             res.render('login')
