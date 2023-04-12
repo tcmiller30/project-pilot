7a71b4fe200518e69390748962a15ffd334bdc08
Danny Nguyen <dannynvn@gmail.com>
2023-04-11 18:05:37 -0700

    fixed bug on logout function for logout.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,4 +1,4 @@
-async function logoutHandler() {
+async function logout() {
     const response = await fetch('api/users/logout', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'}
