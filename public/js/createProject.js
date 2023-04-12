21614e62f695781afbbf37c71a6ca74d31eafbb4
Aubrey Johnston <aubreyjohnston@Aubreys-MacBook-Air.local>
2023-04-11 18:33:51 -0700

    fixed some routig bugs

--- public/js/createProject.js
+++ public/js/createProject.js
@@ -2,20 +2,20 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#projectName').value.trim();
+    const title = document.querySelector('#projectName').value.trim();
     const clientName = document.querySelector('#clientName').value.trim();
     const description = document.querySelector('#projectDescription').value.trim();
-    const dueDate = document.querySelector('#projectDue').value.trim();
+    const project_due = document.querySelector('#projectDue').value.trim();
     const hours = document.querySelector('#projectHours').value.trim();
     const rate = document.querySelector('#projectRate').value.trim();
   
     // gathers user input for project data and then sends a POST request to the API endpoint
-    console.log({name, clientName, description, dueDate, hours, rate});
-    if (name && clientName && description && dueDate && hours && rate) {
+    console.log({title, clientName, description, project_due, hours, rate});
+    if (title && clientName && description && project_due && hours && rate) {
       
       const response = await fetch(`/api/projects`, {
         method: 'POST',
-        body: JSON.stringify({ name, clientName, description, dueDate, hours, rate }),
+        body: JSON.stringify({ title, clientName, description, project_due, hours, rate }),
         headers: {
           'Content-Type': 'application/json',
         },
@@ -30,6 +30,4 @@ const newFormHandler = async (event) => {
   };
 
 // event listener for the create project form
-document
-.querySelector('#projectForm')
-.addEventListener('submit', newFormHandler);
+document.querySelector('#projectForm').addEventListener('submit', newFormHandler);
